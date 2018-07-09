f95c9abca3d76e0c073081c92f0da25f404ab392
alonreches <alon.reches@mail.huji.ac.il>
2018-07-09 15:22:54 +0300

    initial commit

---
+++ WikiProblem.py
@@ -0,0 +1,34 @@
+import wikipedia
+
+
+class WikiProblem:
+    def __init__(self, start_state, goal_state):
+        self.start_state = wikipedia.page(start_state)
+        self.goal_state = wikipedia.page(goal_state)
+
+    def get_start_state(self):
+        return self.start_state.title
+
+    def get_goal_state(self):
+        return self.goal_state.title
+
+    def is_goal_state(self, article):
+        try:
+            return wikipedia.page(article).title == self.goal_state.title
+        except Exception as E:
+            return False
+
+    def get_successors(self, article):
+        try:
+            return wikipedia.page(article).links
+        except Exception as E:
+            return []
+
+    def get_predecessor(self):
+        raise Exception("Not Implemented! how to find this?")
+
+    def get_categories_of_article(self, article):
+        return wikipedia.page(article).categories
+
+    def run_dfs(self):
+        raise Exception("Not Implemented yet!")
---
+++ WikiSolver.py
@@ -0,0 +1,89 @@
+# TODO: don't make it look like yoni's solution
+from WikiProblem import WikiProblem
+import util
+
+
+class Node:
+    def __init__(self, state, parent=None, action=None, path_cost=0):
+        self.state = state
+        self.parent = parent
+        self.action = action
+        if parent:
+            self.path_cost = parent.path_cost + path_cost
+            self.depth = parent.depth + 1
+        else:
+            self.path_cost = path_cost
+            self.depth = 0
+
+    def __repr__(self):
+        return '<Node %s>' % (self.state,)
+
+    def nodePath(self):
+        x, result = self, [self]
+        while x.parent:
+            result.append(x.parent)
+            x = x.parent
+
+        result.reverse()
+        return result
+
+    def path(self):
+        actions = []
+        currnode = self
+        while currnode.parent:
+            actions.append(currnode.action)
+            currnode = currnode.parent
+
+        actions.reverse()
+        return actions
+
+    def expand(self, problem):
+        return [Node(next, self) for next in problem.get_successors(self.state)]
+
+
+REVERSE_PUSH = False
+
+
+def graph_search(problem, fringe):
+    start_state = problem.get_start_state()
+    fringe.push(Node(problem.get_start_state()))
+    try:
+        start_state.__hash__()
+        visited = set()
+    except:
+        visited = list()
+
+    while not fringe.isEmpty():
+        node = fringe.pop()
+        if problem.is_goal_state(node.state):
+            return node.path()
+        try:
+            in_visited = node.state in visited
+        except:
+            visited = list(visited)
+            in_visited = node.state in visited
+
+        if not in_visited:
+            if isinstance(visited, list):
+                visited.append(node.state)
+            else:
+                visited.add(node.state)
+            next_nodes = node.expand(problem)
+            if REVERSE_PUSH:
+                next_nodes.reverse()
+            for next_node in next_nodes:
+                fringe.push(next_node)
+    return
+
+
+def null_heuristic(state, problem=None):
+    return 0
+
+
+def a_star_search(problem, heuristic=null_heuristic):
+    return graph_search(problem,
+                        util.PriorityQueueWithFunction(lambda node: node.path_cost + heuristic(node.state, problem)))
+
+if __name__ == "__main__":
+    problem = WikiProblem("Cherry", "Fruit")
+    print(a_star_search(problem=problem))
